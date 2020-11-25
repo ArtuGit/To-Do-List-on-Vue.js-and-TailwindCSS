27d683f6e086f11bfda749b51e09929903a9ce9d
Artem L <artuon@gmail.com>
2020-11-25 17:29:51 +0200

    Lint

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -52,9 +52,6 @@ export default {
     }
   }
 };
-
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-import '../assets/css/tailwind.css'
+import "../assets/css/tailwind.css";
 
 import NewTask from "./components/NewTask.vue";
 import Task from "./components/Task.vue";
